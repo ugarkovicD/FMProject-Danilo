c8e0468022529fb3798b4663d68be9985707c33e
ugarkovicD <du05249718@priestley.ac.uk>
2021-03-24 11:49:18 +0000

    Revert "Merge remote-tracking branch 'origin/KieranFixingLevelGeneration' into main"
    This reverts commit 49362f153db556c1957e4b159dd3068b21399937, reversing
    changes made to 5e96898e989279d93e9a1ea0f9a0fc5e8d1fb80f.

---
+++ Assets/Code/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    private Transform Player;
+    private Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(Player.position.x, Player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
